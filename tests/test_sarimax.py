8301cddf2940add16fe8a6d8b3ee3efc547f33ca
valcilio <eugeniovalcilio@gmail.com>
2022-08-04 08:49:50 -0300

    updating tests and requirements.txt

--- tests/test_sarimax.py
+++ tests/test_sarimax.py
@@ -15,11 +15,11 @@ def test_forecast_sarimax():
     
     df_pred = ts_model.forecast()
 
-    return assert_frame_equal(df_test_pred, df_pred)
+    assert_frame_equal(df_test_pred, df_pred)
 
 def test_forecast_sarimax_with_model_passed():
     '''Test forecast with passing a fitted model'''
 
     df_pred = ts_model.forecast(fitted_model=model)
 
-    return assert_frame_equal(df_test_pred, df_pred)
+    assert_frame_equal(df_test_pred, df_pred)
